96b94c1f68cbbae29103def89eb8eabc559d4886
Masami Yonehara <zeitdiebe@gmail.com>
2018-12-10 19:56:40 +0900

    Add test cases for expression combinations

---
+++ test/combinations.ts
@@ -0,0 +1,14 @@
+import Choice from '../src/choice'
+import Sequence from '../src/sequence'
+import Terminal from '../src/terminal'
+
+test('parse returns true if inputs is equal to the character which was given to constructor.', () => {
+  const parser = new Choice([
+    new Sequence([new Terminal('a'), new Terminal('b')]),
+    new Sequence([new Terminal('A'), new Terminal('B')]),
+  ])
+
+  expect(parser.parse('ab')).toEqual(true)
+  expect(parser.parse('AB')).toEqual(true)
+  expect(parser.parse('aB')).toEqual(false)
+})
