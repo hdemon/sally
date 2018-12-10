0444a697edf3648149b694983f2b3df4209aa37b
Masami Yonehara <zeitdiebe@gmail.com>
2018-12-10 20:01:07 +0900

    Refactor tests

--- test/choice.ts
+++ test/choice.ts
@@ -1,17 +1,13 @@
 import Choice from '../src/choice'
 import Terminal from '../src/terminal'
 
-test('parse returns true if inputs is equal to the character which was given to constructor.', () => {
-  const result = new Choice([new Terminal('a'), new Terminal('b')]).parse('a')
-  expect(result).toEqual(true)
-})
-
-test("parse return false if inputs isn't equal to the character which was given to constructor.", () => {
-  const result = new Choice([new Terminal('a'), new Terminal('b')]).parse('b')
-  expect(result).toEqual(true)
-})
+const parser = new Choice([new Terminal('a'), new Terminal('b')])
 
 test('parse returns true if inputs is equal to the character which was given to constructor.', () => {
-  const result = new Choice([new Terminal('a'), new Terminal('b')]).parse('c')
-  expect(result).toEqual(false)
+  expect(parser.parse('a')).toEqual(true)
+  expect(parser.parse('b')).toEqual(true)
+})
+
+test("parse returns false if inputs isn't equal to the character which was given to constructor.", () => {
+  expect(parser.parse('c')).toEqual(false)
 })
--- test/sequence.ts
+++ test/sequence.ts
@@ -1,16 +1,12 @@
 import Sequence from '../src/sequence'
 import Terminal from '../src/terminal'
 
+const parser = new Sequence([new Terminal('a'), new Terminal('b')])
+
 test('parse returns true if inputs is equal to the character which was given to constructor.', () => {
-  const result = new Sequence([new Terminal('a'), new Terminal('b')]).parse(
-    'ab'
-  )
-  expect(result).toEqual(true)
+  expect(parser.parse('ab')).toEqual(true)
 })
 
 test("parse return false if inputs isn't equal to the character which was given to constructor.", () => {
-  const result = new Sequence([new Terminal('a'), new Terminal('b')]).parse(
-    'a1'
-  )
-  expect(result).toEqual(false)
+  expect(parser.parse('AB')).toEqual(false)
 })
