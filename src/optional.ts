bf586368e8a2feca094100127a5ec269ad3eef78
Masami Yonehara <zeitdiebe@gmail.com>
2018-12-10 21:34:28 +0900

    Refactor Optional class

--- src/optional.ts
+++ src/optional.ts
@@ -1,27 +1,17 @@
 import Empty from '../src/empty'
 import NonTerminal from '../src/non_terminal'
 import Terminal from '../src/terminal'
+import Choice from './choice'
 type ParsingExpression = Terminal | NonTerminal | Empty
 
 export default class Optional implements NonTerminal {
   private parsingExpression: ParsingExpression
 
   constructor(parsingExpression: ParsingExpression) {
-    this.parsingExpression = parsingExpression
+    this.parsingExpression = new Choice([parsingExpression, new Empty()])
   }
 
   public parse(input: string): boolean {
-    return this.__Parse(input)
-  }
-
-  private __Parse(input: string): boolean {
-    const parseResult = this.parsingExpression.parse(input)
-    if (parseResult === true) {
-      return true
-    } else if (input === '') {
-      return true
-    } else {
-      return false
-    }
+    return this.parsingExpression.parse(input)
   }
 }
