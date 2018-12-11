c3e2a6a0ccfb8d6dcd7f0f986b86dd46e5198d80
Masami Yonehara <zeitdiebe@gmail.com>
2018-12-12 00:30:32 +0900

    Fix test cases for Optional

--- test/terminal.ts
+++ test/terminal.ts
@@ -1,12 +1,19 @@
 import Terminal from '../src/terminal'
 
 test('parse returns true if inputs is equal to the character which was given to constructor.', () => {
-  expect(new Terminal('1').parse('1')).toEqual(true)
-  expect(new Terminal('123').parse('123')).toEqual(true)
+  expect(new Terminal('1').parse('1')).toEqual({ success: true, consumed: 1 })
+  expect(new Terminal('123').parse('123')).toEqual({
+    consumed: 3,
+    success: true,
+  })
 })
 
 test("parse return false if inputs isn't equal to the character which was given to constructor.", () => {
-  expect(new Terminal('A').parse('1')).toEqual(false)
+  expect(new Terminal('A').parse('1')).toEqual({ success: false, consumed: 0 })
+  expect(new Terminal('123').parse('1')).toEqual({
+    consumed: 0,
+    success: false,
+  })
 })
 
 // expect(new Terminal('123').parse('12')).toEqual(false)
