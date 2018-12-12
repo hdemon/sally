00c5cc7624e9fd3e7232940beaa2b9f16e70b2bf
Masami Yonehara <zeitdiebe@gmail.com>
2018-12-12 20:56:52 +0900

    Make Choice return actual the number of consumed

--- src/choice.ts
+++ src/choice.ts
@@ -23,13 +23,13 @@ export default class Choice implements IParsingExpression {
       if (result.consumed === input.length) {
         return { success: true, consumed: result.consumed }
       } else {
-        return { success: false, consumed: 0 }
+        return { success: false, consumed: result.consumed }
       }
     } else {
       if (index < this.lazyParsingExpressions.length - 1) {
         return this.__Parse(index + 1, input)
       } else {
-        return { success: false, consumed: 0 }
+        return { success: false, consumed: result.consumed }
       }
     }
   }
