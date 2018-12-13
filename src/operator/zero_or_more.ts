6246734a6171d7096a5fb3df04ca948cd70bc9fa
Masami Yonehara <zeitdiebe@gmail.com>
2018-12-14 02:56:29 +0900

    Rewrite ZeroOrMore because I avoided implement lazy evaluation

--- src/operator/zero_or_more.ts
+++ src/operator/zero_or_more.ts
@@ -1,33 +1,46 @@
 import { choice } from './choice'
 import { empty } from './empty'
 import l from '../core/logger'
-import {
-  IParsingExpression,
-  LazyParsingExpression,
-} from '../parsing_expression'
+import { ParsingExpression } from '../core/parsing_expression'
 import { sequence } from './sequence'
 
-export default class ZeroOrMore implements IParsingExpression {
-  private parsingExpression: LazyParsingExpression
+export default class ZeroOrMore implements ParsingExpression {
+  private parsingExpression: ParsingExpression
+  private consumed: number
 
-  constructor(lazyParsingExpression: LazyParsingExpression) {
-    this.parsingExpression = choice([
-      sequence([lazyParsingExpression, zeroOrMore(lazyParsingExpression)]),
-      empty(),
-    ])
+  constructor(parsingExpression: ParsingExpression) {
+    this.parsingExpression = parsingExpression
+    // this.parsingExpression = choice([
+    //   sequence([parsingExpression, zeroOrMore(parsingExpression)]),
+    //   empty(),
+    // ])
   }
 
   public parse(input: string): { success: boolean; consumed: number } {
-    const result = this.parsingExpression().parse(input)
+    this.consumed = 0
+    return this.__Parse(input, 0)
+  }
+
+  private __Parse(
+    input: string,
+    offset: number = 0
+  ): { success: boolean; consumed: number } {
+    const stringToTry = input.slice(offset)
+    const result = this.parsingExpression.parse(stringToTry)
     l({
       input,
       nameOfExpression: 'zero_or_more',
       result,
     })
-    return { ...result }
+    this.consumed += result.consumed
+
+    if (result.success === true) {
+      return this.__Parse(input, this.consumed)
+    } else {
+      return { success: true, consumed: this.consumed }
+    }
   }
 }
 
-export const zeroOrMore = (
-  lazyParsingExpression: LazyParsingExpression
-) => () => new ZeroOrMore(lazyParsingExpression)
+export const zeroOrMore = (parsingExpression: ParsingExpression) =>
+  new ZeroOrMore(parsingExpression)
