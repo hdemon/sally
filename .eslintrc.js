19c4322123d5ce6fa37fdf0d51f510b6b97a8c43
Masami Yonehara <masami.yonehara@linecorp.com>
2019-09-12 14:13:46 +0900

    Modify .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,20 +9,10 @@ module.exports = {
   plugins: ['@typescript-eslint', 'prettier'],
   parser: '@typescript-eslint/parser',
   env: {
-    es6: true,
-    browser: true,
-    commonjs: true,
     node: true,
-    jquery: true,
     jest: true,
   },
   rules: {
     'prettier/prettier': 'error',
-  },
-  globals: {
-    shallow: true,
-    render: true,
-    mount: true,
-    LCS: true,
   },
 }
